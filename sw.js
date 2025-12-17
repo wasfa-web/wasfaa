5db4304ce7114fade2403c19426087d115de1bb2
wasfa-web <momaoalo@gmail.com>
2025-12-17 16:43:53 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,24 @@
-const CACHE_NAME = "recipes-app-v1";
+const CACHE = "recipes-v1";
 const FILES = [
   "./",
   "./index.html",
+  "./app.css",
   "./app.js",
-  "./app.css"
+  "./manifest.json"
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
   );
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
-  );
+self.addEventListener("fetch", e => {
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
-
