bad88218e4db659ef3892af6a4df0b7ba450bf7f
wasfa-web <momaoalo@gmail.com>
2025-12-17 15:55:30 +0300

    Implement service worker for caching files

--- sw.js
+++ sw.js
@@ -1,1 +1,20 @@
+const CACHE_NAME = "recipes-app-v1";
+const FILES = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./app.css"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(res => res || fetch(event.request))
+  );
+});
 
