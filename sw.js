e6a2dae933710a6c796a984ee4b5e0b151e4b5c7
wasfa-web <momaoalo@gmail.com>
2025-12-17 21:29:26 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,24 @@
 const CACHE = "recipes-v2";
+
 const FILES = [
   "./",
   "./index.html",
+  "./all_recipes.html",
   "./app.css",
   "./app.js",
-  "./manifest.json"
+  "./all_recipes.js",
+  "./manifest.json",
+  "./recipe-icon.png"
 ];
 
 self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
 });
 
 self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
